77dd8cd4b3f29fac146da8a5873ce35e489ea5ca
linuxrafiq <rafiq.i@live.co.uk>
2020-05-22 20:02:21 +0600

    created module for tour route and user route, move code from app.js to tourRouter and userRoute, then exporting them

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
+const tourRouter = require('./route/tourRouter');
+const userRouter = require('./route/userRouter');
 const app = express();
 //using morgan to logging middleware
 //options: combined , common , dev, short ,tiny
 app.use(morgan('short'));
 //creating our own middleware function
-app.use((req, res, next) => {
+app.use((req, res, next) => {   
   // console.log(req);
   console.log('hello from the middleware');
   next(); // next is function provided by express, if I do not call next middleware pipline will broke.
@@ -29,149 +29,6 @@ app.use(
 
 //this is a middleware function
 app.use(bodyParser.json()); // to support JSON-encoded bodies
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    requestedAt: req.requestTime,
-    data: {
-      tours: tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  const id = req.params.id * 1; // mulltiply by 1 will convert the string to int
-  const tour = tours.find((el) => el.id === id);
-  //if(id>tours.length){
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: tour,
-    },
-  });
-};
-const createTour = (req, res) => {
-  console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
- res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>',
-    },
-  });
-};
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-const getAllUsers = (req, res)=>{
-    res.status('200').json({
-        status:'error',
-        message:'This route is not yet defined'
-    });
-}
-const createUser = (req, res)=>{
-    res.status('500').json({
-        status:'error',
-        message:'This route is not yet defined'
-    });
-}
-const getUser = (req, res)=>{
-    res.status('500').json({
-        status:'error',
-        message:'This route is not yet defined'
-    });
-}
-
-const updateUser = (req, res)=>{
-    res.status('500').json({
-        status:'error',
-        message:'This route is not yet defined'
-    });
-}
-
-const deleteUser = (req, res)=>{
-    return res.status('500').json({
-        status:'error',
-        message:'This route is not yet defined'
-    });
-}
-
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-/**
- * app.get('/api/v1/tours', getAllTours);
- * app.post('/api/v1/tours', createTour);
- * in one call below
- */
-tourRouter.route('/').get(getAllTours).post(createTour);
-
-/**
- * app.get('/api/v1/tours/:id', getTour);
- * app.patch('/api/v1/tours/:id', updateTour);
- * app.delete('/api/v1/tours/:id', deleteTour)
- *
- * in one call all three below
- */
-tourRouter
-  .route('/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-
-//Users route
-app.route('/').get(getAllUsers).post(createUser);
-app
-  .route('/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
