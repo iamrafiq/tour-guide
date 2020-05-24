2cceea022956422c10ef4e42ec43968962a4c547
linuxrafiq <rafiq.i@live.co.uk>
2020-05-24 14:49:50 +0600

    created config.env file

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 // database configuration, error handling stuff, env variable
 const app = require('./app');
 //starting server
+console.log(app.get('env'));
+console.log(process.env)
 const port = 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
