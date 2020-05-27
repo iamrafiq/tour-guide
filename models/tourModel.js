2850415be3db58bded41b087907b3fbe2a13f9a1
linuxrafiq <rafiq.i@live.co.uk>
2020-05-28 00:34:51 +0600

    built in validator max, min enum, maxLenght, minLenght

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'], //woks only for strings
+      minlength: [10, 'A tour name must have more or equal then 10 characters'], // works only for strings
     },
     slug: String,
     duration: {
@@ -21,10 +23,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        // works only for string
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty level is either: easy, medium, or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'], // works for number and dates
+      max: [5, 'Rating must be below 5.0'], // works for number and dates
     },
     ratingsQuantity: {
       type: Number,
